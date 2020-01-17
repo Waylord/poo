59456e97acd47e3ef79e415e2580cbbccd48badb
Jair L. Ribeiro <42625377+Waylord@users.noreply.github.com>
2020-01-17 17:20:22 -0300

    Update Main.java

--- java/Lista/01/Questao05/Main.java
+++ java/Lista/01/Questao05/Main.java
@@ -1,3 +1,8 @@
+/*
+Este programa imprime a tabuada de multiplicação de 1 a 10,
+inclusives, pulando uma linha entre os blocos.
+*/
+
 public class Main {
     public static void main(String[] args){
         for(int i = 1; i <= 10; i++){
