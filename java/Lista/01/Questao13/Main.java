3cfbb95e974f533364b0f34ee6f5eb2fb86e8b3b
Jair L. Ribeiro <42625377+Waylord@users.noreply.github.com>
2020-01-22 18:32:15 -0300

    Update Main.java

--- java/Lista/01/Questao13/Main.java
+++ java/Lista/01/Questao13/Main.java
@@ -1,12 +1,14 @@
 package Questao13;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         int[] vetor = new int[100];
+        Random random = new Random();
         for(int i = 0; i < 100; i++){
-            vetor[i] = (int) (Math.random() * 100);
+            vetor[i] = (random.nextInt());
         }
         int aux;
         for(int x = 0; x < 100; x++){
