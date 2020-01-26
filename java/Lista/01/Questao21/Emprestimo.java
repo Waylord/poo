f2cb62602abe2019262a675fe3dd3bc915da40e2
Jair L. Ribeiro <42625377+Waylord@users.noreply.github.com>
2020-01-26 18:37:17 -0300

    Update Emprestimo.java

--- java/Lista/01/Questao21/Emprestimo.java
+++ java/Lista/01/Questao21/Emprestimo.java
@@ -1,7 +1,36 @@
+package Questao21;
+
+
+
 public class Emprestimo {
-    public boolean checaDisponibilidade;
-    
-    public Emprestimo(){
-        
+
+    public Emprestimo(Livro book, Pessoa people){
+        realizarEmprestimo(book, people);
+    }
+
+    public void realizarEmprestimo(Livro book, Pessoa people) {
+        if(people.verificaEmprestimoAtivo() == true) {
+            System.out.println("O usuário " + people.nome + " possui um empréstimo ativo. Não é possível realizar o empréstimo de mais de um livro ao mesmo tempo.");        }
+        else if(book.estaDisponivel() == false) {
+            System.out.println("O livro "+ book.livro + " não está disponível. Atualmente este livro está com " + book.emprestadoPara);
+        }
+        else {
+            book.disponivel = false;
+            people.possuiEmprestimo = true;
+            book.emprestadoPara = people.nome;
+            people.livroEmprestado = book.livro;
+            System.out.println("O livro " + book.livro + " foi emprestado para " + people.nome + " com sucesso.");
+
+        }
+    }
+
+    public void devolverEmprestimo(Livro book, Pessoa people) {
+        if(people.verificaEmprestimoAtivo() == true) {
+            people.possuiEmprestimo = false;
+            book.emprestadoPara = "";
+            people.livroEmprestado = "";
+            book.disponivel = true;
+            System.out.println("O livro " + book.livro + " foi devolvido com sucesso por " + people.nome);
+        }
     }
 }
