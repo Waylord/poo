6ab74804f8ad4bf4cabfabf01672142a053a5075
Jair L. Ribeiro <42625377+Waylord@users.noreply.github.com>
2020-01-28 16:31:11 -0300

    Create Main.java

---
+++ java/Lista/01/Questao20/Main.java
@@ -0,0 +1,17 @@
+package Questao20;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args){
+		final int TAM = 3;
+		
+		Agenda principal = new Agenda();
+		Contato[] lista;
+		for(int i = 0; i < TAM; i++) {
+			lista = new Contato;
+		}
+		
+		
+	}
+}
