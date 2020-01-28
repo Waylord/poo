830e015dd45546b1ebaaf534afd1f2fdc318c860
Jair L. Ribeiro <42625377+Waylord@users.noreply.github.com>
2020-01-28 16:31:00 -0300

    Create Contato.java

---
+++ java/Lista/01/Questao20/Contato.java
@@ -0,0 +1,22 @@
+package Questao20;
+
+import java.util.Scanner;
+
+public class Contato {
+	private String nomeContato;
+	private long telefone;
+	Scanner t = new Scanner(System.in);
+		
+	public String verNomeContato()
+		return nomeContato;
+	
+	public int verTelefoneContato()
+		return telefone;
+	
+	public int addContato(){
+		System.out.print("Informe o nome do contato: ");
+		this.nomeContato = ;
+	}
+	
+	
+}
