eb364340fcffa9255b457e2048eee49b0fe62647
Jair L. Ribeiro <42625377+Waylord@users.noreply.github.com>
2020-02-01 19:56:13 -0300

    Update Main.java

--- java/Lista/02/Questao01/Main.java
+++ java/Lista/02/Questao01/Main.java
@@ -2,8 +2,21 @@ package Questao01;
 
 import java.util.Scanner;
 
-public class Main {
+Public class Main {
     public static void main (String[] args) {
-        
+        Pessoa filho = new Pessoa(),
+               mae = new Pessoa (),
+               pai = new Pessoa (),
+               avoMaterna = new Pessoa (),
+               avoMaterno = new Pessoa (),
+               avoPaterna = new Pessoa (),
+               avoPaterno = new Pessoa ();
+
+        filho.associar Pai (pai);
+        filho.associar Mae (mae);
+        mae.associar Pai (avô Materno);
+        mae.associar Mãe (avó Materna);
+        pai.associar Pai (avô Paterno);
+        pai.associar Mãe (avó Paterna);
     }
 }
