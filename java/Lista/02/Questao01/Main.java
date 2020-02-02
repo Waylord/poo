291763c856ca1819cdf0ab967e5c2ef5523dbdae
Jair L. Ribeiro <42625377+Waylord@users.noreply.github.com>
2020-02-02 15:46:58 -0300

    Update Main.java

--- java/Lista/02/Questao01/Main.java
+++ java/Lista/02/Questao01/Main.java
@@ -1,22 +1,37 @@
-package Questao01;
+/*
+Faça um programa para representar a árvore genealógica de uma família. Para tal, crie
+uma classe Pessoa que permita indicar, além de nome e idade, o pai e a mãe. Tenha em
+mente que pai e mãe também são do tipo Pessoa.
+*/
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
-        Pessoa filho = new Pessoa(),
-               mae = new Pessoa (),
-               pai = new Pessoa (),
-               avoMaterna = new Pessoa (),
-               avoMaterno = new Pessoa (),
-               avoPaterna = new Pessoa (),
-               avoPaterno = new Pessoa ();
+	public static void main(String[] args) {
+		Pessoa filho = new Pessoa(),
+			   mae = new Pessoa(),
+			   pai = new Pessoa(),
+			   avoMaterna = new Pessoa(),
+			   avoMaterno = new Pessoa(),
+			   avoPaterna = new Pessoa(),
+			   avoPaterno = new Pessoa();
+		
+		filho.associarPai(pai);
+		filho.associarMae(mae);
+		mae.associarPai(avoMaterno);
+		mae.associarMae(avoMaterna);
+		pai.associarPai(avoPaterno);
+		pai.associarMae(avoPaterna);
 
-        filho.associar Pai (pai);
-        filho.associar Mae (mae);
-        mae.associar Pai (avô Materno);
-        mae.associar Mãe (avó Materna);
-        pai.associar Pai (avô Paterno);
-        pai.associar Mãe (avó Paterna);
-    }
+        System.out.println("Último descendente: " + filho.getNomePessoa());
+        System.out.println();
+        System.out.println("Pai: " + filho.mostrarNomePai());
+        System.out.println("Avô paterno: " + pai.mostrarNomePai());
+        System.out.println("Avó paterna: " + pai.mostrarNomeMae());
+        System.out.println();
+        System.out.println("Mãe: " + filho.mostrarNomeMae());
+        System.out.println("Avô materno: " + mae.mostrarNomePai());
+        System.out.println("Avó materna: " + mae.mostrarNomeMae());
+        
+	}
 }
